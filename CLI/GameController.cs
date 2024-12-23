a64c728d6aa40dadae1c55f39601e36d950d32c4
ya'akov <diogobonofre@gmail.com>
2024-12-23 09:05:26 -0300

    feat: provided better provisory engine interface in the game controller class

--- CLI/GameController.cs
+++ CLI/GameController.cs
@@ -1,4 +1,67 @@
-﻿namespace CLI {
-  internal class GameController {
+﻿using CLI.Pieces;
+
+namespace CLI {
+  public class GameController {
+    public List<Player> Players { get; private set; } = [];
+    public Color Turn { get; set; } = Color.White;
+    public Board Board { get; set; } = new Board();
+
+    public GameController() { }
+
+    public GameController(Board board, List<Player> players) {
+      Board = board;
+      Players = players;
+    }
+
+    public void RefreshScreen() {
+      Console.Clear();
+      UI.PlayersStatus(Players);
+      UI.ShowBoard(Board);
+    }
+
+    public void GivePlayerPieces() {
+      foreach (var p in Players) {
+        if (p.Color == Color.White) {
+          p.Pieces = [
+            new Rook(new Coordinate(0, 0), Color.White),
+            new Knight(new Coordinate(0, 1), Color.White),
+            new Bishop(new Coordinate(0, 2), Color.White),
+            new King(new Coordinate(0, 3), Color.White),
+            new Queen(new Coordinate(0, 4), Color.White),
+            new Bishop(new Coordinate(0, 5), Color.White),
+            new Knight(new Coordinate(0, 6), Color.White),
+            new Rook(new Coordinate(0, 7), Color.White),
+            new Pawn(new Coordinate(1, 0), Color.White),
+            new Pawn(new Coordinate(1, 1), Color.White),
+            new Pawn(new Coordinate(1, 2), Color.White),
+            new Pawn(new Coordinate(1, 3), Color.White),
+            new Pawn(new Coordinate(1, 4), Color.White),
+            new Pawn(new Coordinate(1, 5), Color.White),
+            new Pawn(new Coordinate(1, 6), Color.White),
+            new Pawn(new Coordinate(1, 7), Color.White)
+          ];
+        }
+        else {
+          p.Pieces = [
+            new Rook(new Coordinate(7, 0), Color.Black),
+            new Knight(new Coordinate(7, 1), Color.Black),
+            new Bishop(new Coordinate(7, 2), Color.Black),
+            new King(new Coordinate(7, 3), Color.Black),
+            new Queen(new Coordinate(7, 4), Color.Black),
+            new Bishop(new Coordinate(7, 5), Color.Black),
+            new Knight(new Coordinate(7, 6), Color.Black),
+            new Rook(new Coordinate(7, 7), Color.Black),
+            new Pawn(new Coordinate(6, 0), Color.Black),
+            new Pawn(new Coordinate(6, 1), Color.Black),
+            new Pawn(new Coordinate(6, 2), Color.Black),
+            new Pawn(new Coordinate(6, 3), Color.Black),
+            new Pawn(new Coordinate(6, 4), Color.Black),
+            new Pawn(new Coordinate(6, 5), Color.Black),
+            new Pawn(new Coordinate(6, 6), Color.Black),
+            new Pawn(new Coordinate(6, 7), Color.Black),
+          ];
+        }
+      }
+    }
   }
 }
--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,105 +1,90 @@
-﻿using CLI.Pieces;
-
-namespace CLI {
+﻿namespace CLI {
   internal static class Program {
     public static void Main() {
       var board = new Board();
 
-      List<Piece> pieces = [
-        new Rook(new Coordinate(0, 0), Color.White),
-        new Knight(new Coordinate(0, 1), Color.White),
-        new Bishop(new Coordinate(0, 2), Color.White),
-        new King(new Coordinate(0, 3), Color.White),
-        new Queen(new Coordinate(0, 4), Color.White),
-        new Bishop(new Coordinate(0, 5), Color.White),
-        new Knight(new Coordinate(0, 6), Color.White),
-        new Rook(new Coordinate(0, 7), Color.White),
-        new Pawn(new Coordinate(1, 0), Color.White),
-        new Pawn(new Coordinate(1, 1), Color.White),
-        new Pawn(new Coordinate(1, 2), Color.White),
-        new Pawn(new Coordinate(1, 3), Color.White),
-        new Pawn(new Coordinate(1, 4), Color.White),
-        new Pawn(new Coordinate(1, 5), Color.White),
-        new Pawn(new Coordinate(1, 6), Color.White),
-        new Pawn(new Coordinate(1, 7), Color.White),
-      ];
-      var player = new Player(Color.White, "Diogo", pieces);
-      for (int i = 0; i < player.Pieces.Count; i++) {
-        var piece = player.Pieces[i];
-        board.SetPieceAt(piece.Position, piece);
-      }
+      var player = new Player(Color.White, "Diogo");
+      var player2 = new Player(Color.Black, "Bruno");
 
-      List<Piece> pieces2 = [
-        new Rook(new Coordinate(7, 0), Color.Black),
-        new Knight(new Coordinate(7, 1), Color.Black),
-        new Bishop(new Coordinate(7, 2), Color.Black),
-        new King(new Coordinate(7, 3), Color.Black),
-        new Queen(new Coordinate(7, 4), Color.Black),
-        new Bishop(new Coordinate(7, 5), Color.Black),
-        new Knight(new Coordinate(7, 6), Color.Black),
-        new Rook(new Coordinate(7, 7), Color.Black),
-        new Pawn(new Coordinate(6, 0), Color.Black),
-        new Pawn(new Coordinate(6, 1), Color.Black),
-        new Pawn(new Coordinate(6, 2), Color.Black),
-        new Pawn(new Coordinate(6, 3), Color.Black),
-        new Pawn(new Coordinate(6, 4), Color.Black),
-        new Pawn(new Coordinate(6, 5), Color.Black),
-        new Pawn(new Coordinate(6, 6), Color.Black),
-        new Pawn(new Coordinate(6, 7), Color.Black),
-      ];
-      var player2 = new Player(Color.Black, "Bruno", pieces2);
-      for (int i = 0; i < player2.Pieces.Count; i++) {
-        var piece = player2.Pieces[i];
-        board.SetPieceAt(piece.Position, piece);
-      }
+      var gameController = new GameController(board: board, players: [player, player2]);
 
-      Console.Clear();
-      Console.WriteLine($"Player 1: {player.Name} - Color: {player.Color}");
-      Console.WriteLine($"Player 2: {player2.Name} - Color: {player2.Color}");
-      Console.WriteLine("For better visualization whites are dark cyan and blacks are dark yellow");
-      UI.ShowBoard(board);
-      Thread.Sleep(1000);
-      Console.Clear();
+      gameController.GivePlayerPieces();
+
+      board.PlacePlayerPieces(player);
+      board.PlacePlayerPieces(player2);
+
+      gameController.RefreshScreen();
 
       board.RemovePieceAt(new Coordinate(6, 0));
-      UI.ShowBoard(board);
-      Thread.Sleep(1000);
-      Console.Clear();
+      gameController.RefreshScreen();
 
       var rook = board.GetPieceAt(new Coordinate(7, 0));
-      if (rook == null) {
-        return;
-      }
-      Move.MovePieceTo(board, rook, new Coordinate(1, 0));
-      UI.ShowBoard(board);
+      if (rook == null) return;
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 0));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 1));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 1));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 2));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 2));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 3));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 3));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 4));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 4));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 5));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 5));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 6));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 6));
+      gameController.RefreshScreen();
       Thread.Sleep(1000);
-      Console.Clear();
-      Move.MovePieceTo(board, rook, new Coordinate(1, 7));
-      UI.ShowBoard(board);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(1, 7));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 7));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 6));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 5));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 4));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 3));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 2));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 1));
+      gameController.RefreshScreen();
+      Thread.Sleep(1000);
+
+      Move.MovePieceTo(board, player2, rook, new Coordinate(0, 0));
+      gameController.RefreshScreen();
+      board.PlacePlayerPieces(player);
+      Thread.Sleep(1000);
     }
   }
 }
