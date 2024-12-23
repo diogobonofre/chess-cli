932b9bdf64276b52f4ad8682f14ebe3bd4c7c091
ya'akov <diogobonofre@gmail.com>
2024-12-23 09:07:41 -0300

    feat: field CapturedPieces in Player to support new PlayerStatus() in UI module

--- CLI/Player.cs
+++ CLI/Player.cs
@@ -1,13 +1,13 @@
 ﻿namespace CLI {
-  internal class Player {
+  public class Player {
     public Color Color { get; set; }
     public string Name { get; set; }
-    public List<Piece> Pieces { get; set; }
+    public List<Piece> Pieces { get; set; } = [];
+    public List<Piece> CapturedPieces { get; set; } = [];
 
-    public Player(Color color, string name, List<Piece> pieces) {
+    public Player(Color color, string name) {
       Color = color;
       Name = name;
-      Pieces = pieces;
     }
   }
 }
