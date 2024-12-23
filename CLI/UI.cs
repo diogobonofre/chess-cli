6f1d2ca277d5ee7ab1f14c7c216bd5204c6ecabd
ya'akov <diogobonofre@gmail.com>
2024-12-23 09:04:53 -0300

    feat: added players status ui module

--- CLI/UI.cs
+++ CLI/UI.cs
@@ -1,5 +1,5 @@
 ﻿namespace CLI {
-  static internal class UI {
+  internal static class UI {
     /// <summary>
     /// Receive a board and print it to the command-line.
     /// </summary>
@@ -8,22 +8,16 @@
       for (int i = 7; i >= 0; i--) {
         Console.Write(i + 1);
         for (int j = 7; j >= 0; j--) {
-          if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
-            Console.BackgroundColor = ConsoleColor.White;
-          } else {
-            Console.BackgroundColor = ConsoleColor.Black;
-          }
+          Console.BackgroundColor = i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1 ? ConsoleColor.White : ConsoleColor.Black;
           var tile = board.Grid[i, j];
           if (tile != null) {
-            if (tile.Color == Color.White) {
-              Console.ForegroundColor = ConsoleColor.DarkCyan;
-            } else {
-              Console.ForegroundColor = ConsoleColor.DarkYellow;
-            }
+            Console.ForegroundColor = tile.Color == Color.White ? ConsoleColor.DarkCyan : ConsoleColor.DarkYellow;
             Console.Write($" {tile.Sprite} ");
-          } else if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
+          }
+          else if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
             Console.Write("   ");
-          } else {
+          }
+          else {
             Console.Write("   ");
           }
           Console.ForegroundColor = ConsoleColor.White;
@@ -32,6 +26,27 @@
         Console.WriteLine();
       }
       Console.WriteLine("  A  B  C  D  E  F  G  H ");
+    }
+
+    /// <summary>
+    /// Receive a player and print it current status to the command-line.
+    /// </summary>
+    /// <param name="player"></param>
+    public static void PlayersStatus(List<Player> players) {
+      foreach (var player in players) {
+        Console.Write($"{player.Name} ({player.Color}): ");
+        foreach (var piece in player.CapturedPieces) {
+          if (player.Color == Color.White) {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+          }
+          else {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+          }
+          Console.Write($"{piece.Sprite} ");
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
+      }
     }
   }
 }
