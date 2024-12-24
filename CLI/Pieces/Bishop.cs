97b2bad8ae597850e3c7143085ab1883b0c5d72d
ya'akov <diogobonofre@gmail.com>
2024-12-24 11:13:03 -0300

    feat: implemented Bishop class movement logic

--- CLI/Pieces/Bishop.cs
+++ CLI/Pieces/Bishop.cs
@@ -3,5 +3,26 @@
     public Bishop(Coordinate position, Color color) : base(position, color) {
       Sprite = '♝';
     }
+
+    public override List<Coordinate> GetPossibleMoves(Board board) {
+      var possibleMoves = new List<Coordinate>();
+
+      for (int i = 1; i < 8; i++) {
+        if (IsWithinBounds(Position.X + i, Position.Y + i)) {
+          possibleMoves.Add(new Coordinate(Position.X + i, Position.Y + i));
+        }
+        if (IsWithinBounds(Position.X + i, Position.Y - i)) {
+          possibleMoves.Add(new Coordinate(Position.X + i, Position.Y - i));
+        }
+        if (IsWithinBounds(Position.X - i, Position.Y + i)) {
+          possibleMoves.Add(new Coordinate(Position.X - i, Position.Y + i));
+        }
+        if (IsWithinBounds(Position.X - i, Position.Y - i)) {
+          possibleMoves.Add(new Coordinate(Position.X - i, Position.Y - i));
+        }
+      }
+
+      return possibleMoves;
+    }
   }
 }
