ab43868108dee2cfc392bdd33821a006bc7e5aa5
ya'akov <diogobonofre@gmail.com>
2024-12-24 19:13:37 -0300

    feat: Rook and Bishop movement logics now detects collisions and stop calculation in such directions

--- CLI/Pieces/Bishop.cs
+++ CLI/Pieces/Bishop.cs
@@ -7,17 +7,26 @@
     public override List<Coordinate> GetPossibleMoves(Board board) {
       var possibleMoves = new List<Coordinate>();
 
+      var topRight = false;
+      var topLeft = false;
+      var bottomRight = false;
+      var bottomLeft = false;
+
       for (int i = 1; i < 8; i++) {
-        if (IsWithinBounds(Position.X + i, Position.Y + i)) {
+        if (!topRight && IsWithinBounds(Position.X + i, Position.Y + i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X + i, Position.Y + i)) != null) topRight = true;
           possibleMoves.Add(new Coordinate(Position.X + i, Position.Y + i));
         }
-        if (IsWithinBounds(Position.X + i, Position.Y - i)) {
+        if (!topLeft && IsWithinBounds(Position.X + i, Position.Y - i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X + i, Position.Y - i)) != null) topLeft = true;
           possibleMoves.Add(new Coordinate(Position.X + i, Position.Y - i));
         }
-        if (IsWithinBounds(Position.X - i, Position.Y + i)) {
+        if (!bottomRight && IsWithinBounds(Position.X - i, Position.Y + i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X - i, Position.Y + i)) != null) bottomRight = true;
           possibleMoves.Add(new Coordinate(Position.X - i, Position.Y + i));
         }
-        if (IsWithinBounds(Position.X - i, Position.Y - i)) {
+        if (!bottomLeft && IsWithinBounds(Position.X - i, Position.Y - i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X - i, Position.Y - i)) != null) bottomLeft = true;
           possibleMoves.Add(new Coordinate(Position.X - i, Position.Y - i));
         }
       }
--- CLI/Pieces/Rook.cs
+++ CLI/Pieces/Rook.cs
@@ -1,5 +1,4 @@
-﻿
-namespace CLI.Pieces {
+﻿namespace CLI.Pieces {
   internal class Rook : Piece {
     public Rook(Coordinate position, Color color) : base(position, color) {
       Sprite = '♜';
@@ -8,17 +7,26 @@ namespace CLI.Pieces {
     public override List<Coordinate> GetPossibleMoves(Board board) {
       var possibleMoves = new List<Coordinate>();
 
+      var top = false;
+      var left = false;
+      var right = false;
+      var bottom = false;
+
       for (int i = 1; i < 8; i++) {
-        if (IsWithinBounds(Position.X + i, Position.Y)) {
+        if (!top && IsWithinBounds(Position.X + i, Position.Y)) {
+          if (board.GetPieceAt(new Coordinate(Position.X + i, Position.Y)) != null) top = true;
           possibleMoves.Add(new Coordinate(Position.X + i, Position.Y));
         }
-        if (IsWithinBounds(Position.X - i, Position.Y)) {
+        if (!bottom && IsWithinBounds(Position.X - i, Position.Y)) {
+          if (board.GetPieceAt(new Coordinate(Position.X - i, Position.Y)) != null) bottom = true;
           possibleMoves.Add(new Coordinate(Position.X - i, Position.Y));
         }
-        if (IsWithinBounds(Position.X, Position.Y + i)) {
+        if (!left && IsWithinBounds(Position.X, Position.Y + i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X, Position.Y + i)) != null) left = true;
           possibleMoves.Add(new Coordinate(Position.X, Position.Y + i));
         }
-        if (IsWithinBounds(Position.X, Position.Y - i)) {
+        if (!right && IsWithinBounds(Position.X, Position.Y - i)) {
+          if (board.GetPieceAt(new Coordinate(Position.X, Position.Y - i)) != null) right = true;
           possibleMoves.Add(new Coordinate(Position.X, Position.Y - i));
         }
       }
