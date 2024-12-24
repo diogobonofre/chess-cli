af1d7d603582f1c8be1f3330286df0fc0ab13b7b
ya'akov <diogobonofre@gmail.com>
2024-12-24 13:16:25 -0300

    feat: implemented Pawn movement logic

--- CLI/Pieces/Pawn.cs
+++ CLI/Pieces/Pawn.cs
@@ -5,7 +5,36 @@
     }
 
     public override List<Coordinate> GetPossibleMoves(Board board) {
-      throw new NotImplementedException();
+      var possibleMoves = new List<Coordinate>();
+
+      if (Color == Color.White) {
+        if (IsWithinBounds(Position.X + 1, Position.Y)) {
+          possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y));
+        }
+        if (IsWithinBounds(Position.X + 1, Position.Y + 1)
+        && board.GetPieceAt(new Coordinate(Position.X + 1, Position.Y + 1)) != null) {
+          possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y + 1));
+        }
+        if (IsWithinBounds(Position.X + 1, Position.Y - 1)
+        && board.GetPieceAt(new Coordinate(Position.X + 1, Position.Y - 1)) != null) {
+          possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y - 1));
+        }
+      }
+      else {
+        if (IsWithinBounds(Position.X - 1, Position.Y)) {
+          possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y));
+        }
+        if (IsWithinBounds(Position.X - 1, Position.Y - 1)
+        && board.GetPieceAt(new Coordinate(Position.X - 1, Position.Y - 1)) != null) {
+          possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y - 1));
+        }
+        if (IsWithinBounds(Position.X - 1, Position.Y + 1)
+        && board.GetPieceAt(new Coordinate(Position.X - 1, Position.Y + 1)) != null) {
+          possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y + 1));
+        }
+      }
+
+      return possibleMoves;
     }
   }
 }
