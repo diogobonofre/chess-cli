29582d81154944d07495c5413af8a3e8ab27ab2b
ya'akov <diogobonofre@gmail.com>
2024-12-24 11:05:52 -0300

    feat: implemented rook movement logic on GetPossibleMoves()

--- CLI/Pieces/Rook.cs
+++ CLI/Pieces/Rook.cs
@@ -1,7 +1,29 @@
-﻿namespace CLI.Pieces {
+﻿
+namespace CLI.Pieces {
   internal class Rook : Piece {
     public Rook(Coordinate position, Color color) : base(position, color) {
       Sprite = '♜';
+    }
+
+    public override List<Coordinate> GetPossibleMoves(Board board) {
+      var possibleMoves = new List<Coordinate>();
+
+      for (int i = 1; i < 8; i++) {
+        if (IsWithinBounds(Position.X + i, Position.Y)) {
+          possibleMoves.Add(new Coordinate(Position.X + i, Position.Y));
+        }
+        if (IsWithinBounds(Position.X - i, Position.Y)) {
+          possibleMoves.Add(new Coordinate(Position.X - i, Position.Y));
+        }
+        if (IsWithinBounds(Position.X, Position.Y + i)) {
+          possibleMoves.Add(new Coordinate(Position.X, Position.Y + i));
+        }
+        if (IsWithinBounds(Position.X, Position.Y - i)) {
+          possibleMoves.Add(new Coordinate(Position.X, Position.Y - i));
+        }
+      }
+
+      return possibleMoves;
     }
   }
 }
