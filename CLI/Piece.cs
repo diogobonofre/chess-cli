49d4ed1188d58e5a41b11c017aecc567a5616b28
diogobonofre <diogobonofre@gmail.com>
2025-01-03 13:15:18 -0300

    doc: added brief documentation to Piece class methods + removed deprecated IsWithinBounds() method

--- CLI/Piece.cs
+++ CLI/Piece.cs
@@ -10,14 +10,23 @@
       Color = color;
     }
 
+    /// <summary>
+    /// Given some board, calculate all possible moves for the current piece instance.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns>List of the possible moves in coordinates</returns>
     public abstract List<Coordinate> GetPossibleMoves(Board board);
 
+    /// <summary>
+    /// Check if a piece contains some coordinate in it's possible moves at a given board.
+    /// </summary>
+    /// <param name="board"></param>
+    /// <param name="position"></param>
+    /// <returns>
+    /// True if the piece can reach the position in one move, if not, false
+    /// </returns>
     public bool IsMoveValid(Board board, Coordinate position) {
       return GetPossibleMoves(board).Contains(position);
-    }
-
-    protected static bool IsWithinBounds(int x, int y) {
-      return x >= 0 && x < 8 && y >= 0 && y < 8;
     }
   }
 }
