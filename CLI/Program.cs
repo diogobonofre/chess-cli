080f36fcba85613bd386c7be2c4f52c006ed40fb
diogobonofre <diogobonofre@gmail.com>
2025-01-03 13:04:15 -0300

    feat: abstracting Main move and capture highlight tests by using the new methods in GameController

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -3,79 +3,21 @@
 namespace CLI {
   internal static class Program {
     public static void Main() {
-      var board = new Board();
-      var player = new Player(Color.White, "Diogo");
+      var whitePlayer = new Player(Color.White, "Diogo");
+      var blackPlayer = new Player(Color.Black, "Pamela");
 
-      var rook = new Rook(new Coordinate(2, 6), Color.White);
-      board.SetPieceAt(rook.Position, rook);
+      var controller = new GameController(whitePlayer, blackPlayer);
 
-      var whiteBishop = new Bishop(new Coordinate(5, 7), Color.White);
-      board.SetPieceAt(whiteBishop.Position, whiteBishop);
+      controller.SetupGame();
 
-      var blackBishop = new Bishop(new Coordinate(5, 5), Color.Black);
-      board.SetPieceAt(blackBishop.Position, blackBishop);
+      var whitePawn = new Pawn(new Coordinate(4, 4), Color.White);
+      controller.Board.SetPieceAt(whitePawn.Position, whitePawn);
 
-      var whitePawn = new Pawn(new Coordinate(2, 0), Color.White);
-      board.SetPieceAt(whitePawn.Position, whitePawn);
+      var blackRook = new Rook(new Coordinate(4, 0), Color.Black);
+      controller.Board.SetPieceAt(blackRook.Position, blackRook);
 
-      var blackPawn = new Pawn(new Coordinate(6, 6), Color.Black);
-      board.SetPieceAt(blackPawn.Position, blackPawn);
-
-      var queen = new Queen(new Coordinate(3, 4), Color.Black);
-      board.SetPieceAt(queen.Position, queen);
-
-      var blackKnight = new Knight(new Coordinate(7, 7), Color.Black);
-      board.SetPieceAt(blackKnight.Position, blackKnight);
-
-      var whiteKnight = new Knight(new Coordinate(4, 4), Color.White);
-      board.SetPieceAt(whiteKnight.Position, whiteKnight);
-
-      var whiteKing = new King(new Coordinate(3, 3), Color.White);
-      board.SetPieceAt(whiteKing.Position, whiteKing);
-
-      var blackKing = new King(new Coordinate(3, 6), Color.Black);
-      board.SetPieceAt(blackKing.Position, blackKing);
-
-      Console.Clear();
-
-      UI.ShowBoard(board, whitePawn);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, blackPawn);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, rook);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, whiteBishop);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, blackBishop);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, whiteKnight);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, blackKnight);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, queen);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, whiteKing);
-      Thread.Sleep(2000);
-      Console.Clear();
-
-      UI.ShowBoard(board, blackKing);
-      Thread.Sleep(2000);
+      UI.ShowBoard(controller, whitePawn);
+      UI.ShowBoard(controller, blackRook);
     }
   }
 }
