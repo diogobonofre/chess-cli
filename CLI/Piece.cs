8cad4c2a9dd5ee10d8d49e73343d0b197ee70b18
diogobonofre <diogobonofre@gmail.com>
2025-01-04 00:50:06 -0300

    refactor: adjuting Piece methods to the new argument pattern

--- CLI/Piece.cs
+++ CLI/Piece.cs
@@ -11,22 +11,22 @@
     }
 
     /// <summary>
-    /// Given some board, calculate all possible moves for the current piece instance.
+    /// Given some game, calculate all possible moves for the current piece instance.
     /// </summary>
-    /// <param name="board"></param>
+    /// <param name="controller"></param>
     /// <returns>List of the possible moves in coordinates</returns>
-    public abstract List<Coordinate> GetPossibleMoves(Board board);
+    public abstract List<Coordinate> GetPossibleMoves(GameController controller);
 
     /// <summary>
-    /// Check if a piece contains some coordinate in it's possible moves at a given board.
+    /// Check if a piece contains some coordinate in it's possible moves at a given game.
     /// </summary>
-    /// <param name="board"></param>
+    /// <param name="controller"></param>
     /// <param name="position"></param>
     /// <returns>
     /// True if the piece can reach the position in one move, if not, false
     /// </returns>
-    public bool IsMoveValid(Board board, Coordinate position) {
-      return GetPossibleMoves(board).Contains(position);
+    public bool IsMoveValid(GameController controller, Coordinate position) {
+      return GetPossibleMoves(controller).Contains(position);
     }
 
     protected static bool IsWithinBounds(int x, int y) {
