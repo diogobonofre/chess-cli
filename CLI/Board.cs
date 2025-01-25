d7394dfbd4f38cdca2bc08ef0ba1459da7469df9
diogobonofre <diogobonofre@gmail.com>
2025-01-25 18:52:43 -0300

    chore/refactor: reformating Board.cs and turning constructor into field initializer for Grid

--- CLI/Board.cs
+++ CLI/Board.cs
@@ -1,31 +1,35 @@
 ﻿namespace CLI;
 
-public class Board {
-  public Piece?[,] Grid { get; private set; }
+public class Board
+{
+    public Piece?[,] Grid { get; private set; } = new Piece[8, 8];
 
-  public Board() {
-    Grid = new Piece[8, 8];
-  }
-
-  public Piece? GetPieceAt(Coordinate position) {
-    return Grid[position.X, position.Y];
-  }
-
-  public void SetPieceAt(Coordinate position, Piece piece) {
-    if (GetPieceAt(position) != null) {
-      Console.WriteLine($"The coordinate ({position.X}, {position.Y}) already has a piece.");
-      return;
+    public Piece? GetPieceAt(Coordinate position)
+    {
+        return Grid[position.X, position.Y];
     }
-    Grid[position.X, position.Y] = piece;
-  }
 
-  public void RemovePieceAt(Coordinate position) {
-    if (GetPieceAt(position) != null) Grid[position.X, position.Y] = null;
-  }
+    public void SetPieceAt(Coordinate position, Piece piece)
+    {
+        if (GetPieceAt(position) != null)
+        {
+            Console.WriteLine($"The coordinate ({position.X}, {position.Y}) already has a piece.");
+            return;
+        }
 
-  public void PlacePlayerPieces(Player player) {
-    foreach (var piece in player.Pieces) {
-      SetPieceAt(piece.Position, piece);
+        Grid[position.X, position.Y] = piece;
     }
-  }
+
+    public void RemovePieceAt(Coordinate position)
+    {
+        if (GetPieceAt(position) != null) Grid[position.X, position.Y] = null;
+    }
+
+    public void PlacePlayerPieces(Player player)
+    {
+        foreach (var piece in player.Pieces)
+        {
+            SetPieceAt(piece.Position, piece);
+        }
+    }
 }
