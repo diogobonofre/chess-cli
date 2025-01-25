da10eb483f7f0277d94b9256f36505ec3901b688
diogobonofre <diogobonofre@gmail.com>
2025-01-25 18:50:41 -0300

    chore: reformating King.cs

--- CLI/Pieces/King.cs
+++ CLI/Pieces/King.cs
@@ -1,39 +1,57 @@
 ﻿namespace CLI.Pieces;
 
-internal class King : Piece {
-  public King(Coordinate position, Color color) : base(position, color) {
-    Sprite = '♚';
-  }
-
-  // TODO: Refactor to reuse existing Bishop and Rook movement logic
-  public override List<Coordinate> GetPossibleMoves(GameController controller) {
-    var possibleMoves = new List<Coordinate>();
-
-    if (IsWithinBounds(Position.X + 1, Position.Y)) {
-      possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y));
-    }
-    if (IsWithinBounds(Position.X - 1, Position.Y)) {
-      possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y));
-    }
-    if (IsWithinBounds(Position.X, Position.Y + 1)) {
-      possibleMoves.Add(new Coordinate(Position.X, Position.Y + 1));
-    }
-    if (IsWithinBounds(Position.X, Position.Y - 1)) {
-      possibleMoves.Add(new Coordinate(Position.X, Position.Y - 1));
-    }
-    if (IsWithinBounds(Position.X + 1, Position.Y + 1)) {
-      possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y + 1));
-    }
-    if (IsWithinBounds(Position.X + 1, Position.Y - 1)) {
-      possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y - 1));
-    }
-    if (IsWithinBounds(Position.X - 1, Position.Y + 1)) {
-      possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y + 1));
-    }
-    if (IsWithinBounds(Position.X - 1, Position.Y - 1)) {
-      possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y - 1));
+public class King : Piece
+{
+    public King(Coordinate position, Color color) : base(position, color)
+    {
+        Sprite = '♚';
     }
 
-    return possibleMoves;
-  }
+    // TODO: Refactor to reuse existing Bishop and Rook movement logic
+    public override List<Coordinate> GetPossibleMoves(GameController controller)
+    {
+        var possibleMoves = new List<Coordinate>();
+
+        if (IsWithinBounds(Position.X + 1, Position.Y))
+        {
+            possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y));
+        }
+
+        if (IsWithinBounds(Position.X - 1, Position.Y))
+        {
+            possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y));
+        }
+
+        if (IsWithinBounds(Position.X, Position.Y + 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X, Position.Y + 1));
+        }
+
+        if (IsWithinBounds(Position.X, Position.Y - 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X, Position.Y - 1));
+        }
+
+        if (IsWithinBounds(Position.X + 1, Position.Y + 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y + 1));
+        }
+
+        if (IsWithinBounds(Position.X + 1, Position.Y - 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X + 1, Position.Y - 1));
+        }
+
+        if (IsWithinBounds(Position.X - 1, Position.Y + 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y + 1));
+        }
+
+        if (IsWithinBounds(Position.X - 1, Position.Y - 1))
+        {
+            possibleMoves.Add(new Coordinate(Position.X - 1, Position.Y - 1));
+        }
+
+        return possibleMoves;
+    }
 }
