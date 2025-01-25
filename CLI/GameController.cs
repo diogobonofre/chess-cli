c2728d73c8fd12f3a8d851137c62aecc66291595
diogobonofre <diogobonofre@gmail.com>
2025-01-25 18:49:26 -0300

    refactor: moved game logic functions from UserInterface.cs to GameController.cs

--- CLI/GameController.cs
+++ CLI/GameController.cs
@@ -77,4 +77,56 @@ public class GameController(Player whitePlayer, Player blackPlayer)
         Board.SetPieceAt(piece.Position, piece);
         player.Pieces.Add(piece);
     }
+
+    /// <summary>
+    /// Check if the current position at the given board is a possible move or capture for the given piece.
+    /// </summary>
+    /// <returns>
+    /// 1: Coordinate is a possible move
+    /// 2: Coordinate is a possible capture
+    /// 0: Coordinate is not a possible move or capture
+    /// </returns>
+    public static int IsPossibleMoveOrCapture(GameController controller, Piece? piece, Coordinate coordinate)
+    {
+        if (piece == null) throw new ArgumentNullException(nameof(piece), message: "piece can't be null.");
+
+        var coordinatePiece = controller.Board.GetPieceAt(coordinate);
+        var possibleMoves = piece.GetPossibleMoves(controller);
+
+        var isCoordinatePossibleMove = possibleMoves.Any(c => c == coordinate);
+
+        return isCoordinatePossibleMove switch
+        {
+            true when coordinatePiece == null => 1,
+            true when piece.Color != coordinatePiece.Color => 2,
+            _ => 0
+        };
+    }
+
+    /// <summary>
+    /// Check if a given piece exists in the given player pieces list and return its index.
+    /// </summary>
+    /// <param name="controller"></param>
+    /// <param name="piece"></param>
+    /// <returns>
+    /// If the pieces exist, return its index (non-negative number).
+    /// Else, return -1 telling that the piece can't be found in the current list.Get
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static int GetPieceIndex(GameController controller, Piece piece)
+    {
+        var player = piece.Color switch
+        {
+            Color.White => controller.WhitePlayer,
+            Color.Black => controller.BlackPlayer,
+            _ => throw new ArgumentOutOfRangeException(piece.Color.ToString())
+        };
+
+        for (var i = 0; i < player.Pieces.Count; i++)
+        {
+            if (ReferenceEquals(piece, controller.WhitePlayer.Pieces[i])) return i;
+        }
+
+        return -1;
+    }
 }
